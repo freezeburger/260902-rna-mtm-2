bcfabe58623b5f84925e1baf5035252ed2a51c55
Renaud Dubuis <renaud.dubuis@decryptage.net>
2026-09-02 14:24:00 +0200

    init

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,131 @@
+import { Image } from 'expo-image';
+import { Link } from 'expo-router';
+import { Platform, StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/hello-wave';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import React, { useMemo } from 'react';
+
+
+/**
+ * Header background color theme for light and dark modes.
+ * Color are set for standard and private application.
+ */
+type HeaderBackgroundThemed = {
+  light: '#A1CEDC';
+  dark: '#1189ad';
+} | {
+  light: '#b9bec0';
+  dark: '#18191a';
+}
+
+const HEADER_BACKGROUND_COLOR: HeaderBackgroundThemed = { light: '#A1CEDC', dark: '#1189ad' };
+
+function HomeScreen( props :any) {
+
+  const shortcuts = useMemo(() => Platform.select({
+    ios: 'cmd + d',
+    android: 'cmd + m',
+    web: 'F12',
+  }), []);
+
+  return (
+
+    <ParallaxScrollView
+      {...props}
+      headerBackgroundColor={HEADER_BACKGROUND_COLOR}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Welcome!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+
+        <ThemedText>
+          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Press{' '}
+          <ThemedText type="defaultSemiBold">
+            {shortcuts}
+          </ThemedText>{' '}
+          to open developer tools.
+        </ThemedText>
+
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <Link href="/modal">
+          <Link.Trigger>
+            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+          </Link.Trigger>
+          <Link.Preview />
+          <Link.Menu>
+            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
+            <Link.MenuAction
+              title="Share"
+              icon="square.and.arrow.up"
+              onPress={() => alert('Share pressed')}
+            />
+            <Link.Menu title="More" icon="ellipsis">
+              <Link.MenuAction
+                title="Delete"
+                icon="trash"
+                destructive
+                onPress={() => alert('Delete pressed')}
+              />
+            </Link.Menu>
+          </Link.Menu>
+        </Link>
+
+        <ThemedText>
+          {`Tap the Explore tab to learn more about what's included in this starter app.`}
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+        <ThemedText>
+          {`When you're ready, run `}
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+export default React.memo(HomeScreen , ( prevProps : any, nextProps : any) => {
+
+  return prevProps.disabled === nextProps.disabled;
+} /* are props equals */ );
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
+
+
+
---
+++ hooks/use-color-scheme.web.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useColorScheme as useRNColorScheme } from 'react-native';
+
+/**
+ * To support static rendering, this value needs to be re-calculated on the client side for web
+ * 
+ * @returns "light" | "dark" | null | undefined 
+ * 
+ * @example
+ * const colorScheme = useColorScheme();
+ * console.log(colorScheme); // "light" | "dark" | null | undefined
+ */
+export function useColorScheme() {
+  const [hasHydrated, setHasHydrated] = useState(false);
+
+  useEffect(() => {
+    setHasHydrated(true);
+  }, []);
+
+  const colorScheme = useRNColorScheme();
+
+  if (hasHydrated) {
+    return colorScheme;
+  }
+
+  return 'light';
+}
