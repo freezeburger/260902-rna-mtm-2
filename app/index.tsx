e10318f08882d85ca998dbfc117790cc15763e30
Renaud Dubuis <renaud.dubuis@decryptage.net>
2026-09-04 09:22:13 +0200

    Setup Mock API

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,8 @@
 import SplashScreen from '@/src/screens/SplashScreen';
 
+fetch('http://10.0.2.2:8080/logs')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error fetching logs:', error));
+
 export default SplashScreen;
